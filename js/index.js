4fc78b661d8534d9464b4dfbe516eeef6de4f20d
Benerman <48377757+Benerman@users.noreply.github.com>
2023-10-29 01:21:58 -0400

    updates to work section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
-$(".projectTitle").on("click", function(){
+
+
+$(".project").on("click", function(){
     console.log(this);
-    $(this).animate({"grid-column": span 6}, 200);
+    $("#highlightedWork").slideDown(200)
+    if ($(this).hasClass("gifts-for-seniors") ) {
+        console.log("has Class gifts-for-seniors")
+        $("#gifts").css("postition", "relative");
+        $("#gifts").css("opacity", "1");
+        
+        $("#usda").css("postition", "absolute");
+        $("#usda").css("opacity", "0");
+
+        $("#gifts").css("display", "flex");
+        $("#usda").css("display", "none");
+    } else {
+        console.log("else")
+        $("#usda").css("opacity", "1");
+        $("#gifts").css("opacity", "0");
+        $("#gifts").css("postition", "absolute");
+        $("#usda").css("postition", "relative");
+        $("#gifts").css("display", "none");
+        $("#usda").css("display", "flex");
+        
+    }
+    $("#highlightedWork").css("position", "relative");
+    $("#highlightedWork").css("opacity", "1");
 });
-// https://css-tricks.com/animating-css-grid-how-to-examples/
